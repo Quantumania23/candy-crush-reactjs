3b449d2050ddb02c9e52f8b25b8bf3259ffed3a3
Quantumania23 <mikepeace981@gmail.com>
2025-02-21 16:26:06 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import {useEffect, useState} from 'react'
 import ScoreBoard from './components/ScoreBoard'
 import blueCandy from './images/blue-candy.png'
